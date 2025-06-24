c48048c4553c65f3d92af4c8b8cfe9a52c23561f
Jacob Kline <jacobk513@gmail.com>
2025-06-23 22:21:45 -0400

    update xcodeproj files for new developer account

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,26 @@
 import UIKit
 import Flutter
 import FirebaseCore
+import Firebase
+import FirebaseMessaging
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+      
+    UNUserNotificationCenter.current().delegate = self
+    application.registerForRemoteNotifications()
+    Messaging.messaging().delegate = self
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM token: \(fcmToken ?? "")")
   }
 }

15f3998d52382e1a33fbd8eb7542f747c98669a8
Jacob Kline <jacobk513@gmail.com>
2025-07-07 21:49:07 -0500

    add initial phone number entry functionality

--- camp_harmony_flutter/lib/components/check_in_page.dart
+++ camp_harmony_flutter/lib/components/check_in_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import '../serverpod_providers.dart';
 
 class CheckInPage extends ConsumerStatefulWidget {
@@ -14,38 +15,53 @@ class CheckInPage extends ConsumerStatefulWidget {
 }
 
 class _CheckInPageState extends ConsumerState<CheckInPage> {
-  CheckInStatus? checkInStatus;
-  bool _checkedIn = false;
-  String errorMessage = '';
-  String statusMessage = '';
+  CheckInStatus? _checkInStatus;
+  bool? _checkedIn = false;
+  String _errorMessage = '';
+  String _statusMessage = '';
+  bool _editingPhoneNumber = false;
+  String? _userPhoneNumber;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   void _invertCheckInStatus(Client client) async {
     try {
-      final CheckInStatus? checkInStatus =
+      if (_userPhoneNumber == null) {
+        setState(() {
+          _errorMessage = "Please enter a valid phone number to check in!";
+        });
+        return;
+      }
+
+      _checkInStatus =
           await client.checkIn.checkIn(0); // Replace 0 with actual user ID
 
-      if (checkInStatus != null) {
+      if (_checkInStatus != null) {
         setState(() {
-          _checkedIn = checkInStatus.checkedIn;
-          this.checkInStatus = checkInStatus;
-          errorMessage = '';
-          statusMessage = checkInStatus.statusMessage ??
+          _checkedIn = _checkInStatus?.checkedIn;
+          _checkInStatus = _checkInStatus;
+          _errorMessage = '';
+          _statusMessage = _checkInStatus?.statusMessage ??
               'Did not receive a status message.';
         });
       } else {
         setState(() {
-          errorMessage = 'Check-in failed. Please try again.';
+          _errorMessage = 'Check-in failed. Please try again.';
         });
       }
     } catch (e) {
       // Handle error
       print('Error checking in: $e');
       setState(() {
-        errorMessage = 'Check-in failed. Please try again.';
+        _errorMessage = 'Check-in failed. Please try again.';
       });
     }
+  }
+
+  void _toggleEditingPhoneNumber() {
+    setState(() {
+      _editingPhoneNumber = !_editingPhoneNumber;
+    });
   }
 
   MaterialTextFormFieldData getMatFormField(String labelText, IconData icon) {
@@ -100,6 +116,31 @@ class _CheckInPageState extends ConsumerState<CheckInPage> {
                           ),
                           textAlign: TextAlign.center,
                         )),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: _editingPhoneNumber
+                          ? PlatformTextField(
+                              textAlign: TextAlign.center,
+                              keyboardType:
+                                  const TextInputType.numberWithOptions(),
+                              readOnly: !_editingPhoneNumber,
+                              hintText: "Phone Number",
+                              onEditingComplete: () {
+                                // TODO: Update this to actually use the value from the field
+                              },
+                            )
+                          : PlatformText(
+                              _userPhoneNumber ?? "No Phone Number Found"),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: PlatformElevatedButton(
+                            onPressed: () {
+                              _toggleEditingPhoneNumber();
+                            },
+                            child: PlatformText(_editingPhoneNumber
+                                ? "Save"
+                                : "Edit Phone Number"))),
                     PlatformElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
@@ -107,28 +148,36 @@ class _CheckInPageState extends ConsumerState<CheckInPage> {
                           _invertCheckInStatus(client);
                         }
                       },
-                      child:
-                          PlatformText(_checkedIn ? 'Check out' : 'Check in'),
+                      child: PlatformText(
+                          _checkedIn == true ? 'Check out' : 'Check in'),
                     ),
-                    const SizedBox(height: 24),
+                    const SizedBox(height: 10),
                     PlatformText(
-                      'You are: ${_checkedIn ? 'Checked In' : 'Checked Out'}',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: _checkedIn ? Colors.green : Colors.red,
-                      ),
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.all(20), child: SignOutButton()),
-                    PlatformText(
-                      statusMessage,
+                      _statusMessage,
                       style: const TextStyle(
-                        fontSize: 18,
+                        fontSize: 15,
                         fontWeight: FontWeight.bold,
                         color: Colors.orange,
                       ),
                     ),
+                    PlatformText(
+                      _errorMessage,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                    ),
+                    PlatformText(
+                      'You are: ${_checkedIn == true ? 'Checked In' : 'Checked Out'}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: _checkedIn == true ? Colors.green : Colors.red,
+                      ),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.all(10), child: SignOutButton()),
                   ],
                 ),
               ),

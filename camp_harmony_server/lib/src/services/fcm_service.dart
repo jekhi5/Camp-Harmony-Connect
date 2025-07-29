1a476ab11f51acf3c66026ecc961fb86a20e1c4f
Jacob Kline <jacobk513@gmail.com>
2025-07-29 14:20:28 -0400

    implements dotenv for fb service account

--- camp_harmony_server/lib/src/services/fcm_service.dart
+++ camp_harmony_server/lib/src/services/fcm_service.dart
@@ -3,10 +3,17 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
+import 'package:dotenv/dotenv.dart';
 
 class FcmService {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+
   Future<String> _getAccessToken() async {
-    final raw = Platform.environment['FIREBASE_SERVICE_ACCOUNT_JSON']!;
+    final raw = env['FIREBASE_SERVICE_ACCOUNT_JSON'];
+    if (raw == null) {
+      print("FIREBASE_SERVICE_ACCOUNT_JSON not present in env");
+      throw Exception("FIREBASE_SERVICE_ACCOUNT_JSON not present in env");
+    }
     final account = json.decode(raw) as Map<String, dynamic>;
     final client = await clientViaServiceAccount(
       ServiceAccountCredentials.fromJson(account),
@@ -21,7 +28,11 @@ class FcmService {
     required String body,
     Map<String, dynamic>? data,
   }) async {
-    final raw = Platform.environment['FIREBASE_SERVICE_ACCOUNT_JSON']!;
+    final raw = env['FIREBASE_SERVICE_ACCOUNT_JSON'];
+    if (raw == null) {
+      print("ERROR: `FIREBASE_SERVICE_ACCOUNT_JSON` not found in environment");
+      return false;
+    }
     final proj = (json.decode(raw) as Map<String, dynamic>)['project_id'];
     final url =
         Uri.parse('https://fcm.googleapis.com/v1/projects/$proj/messages:send');

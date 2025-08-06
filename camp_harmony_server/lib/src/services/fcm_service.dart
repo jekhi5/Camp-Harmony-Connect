27205ac279ef113a41ad80b04e67c5dac22fa342
Jacob Kline <jacobk513@gmail.com>
2025-08-06 19:20:10 -0400

    adds documentation to enedpoints and prunes development comments

--- camp_harmony_server/lib/src/services/fcm_service.dart
+++ camp_harmony_server/lib/src/services/fcm_service.dart
@@ -1,17 +1,23 @@
-// server/lib/src/services/fcm_service.dart
 import 'dart:convert';
 import 'dart:io';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
 import 'package:dotenv/dotenv.dart';
+import 'package:serverpod/serverpod.dart';
 
+/// Service for sending Firebase Cloud Messages (FCM)
 class FcmService {
   var env = DotEnv(includePlatformEnvironment: true)..load();
 
-  Future<String> _getAccessToken() async {
+  /// Get an access token for Firebase service account
+  /// This token is used to authenticate requests to the FCM API.
+  /// Throws an exception if the service account JSON is not found in the environment.
+  /// The service account JSON should be stored in the environment variable `FIREBASE_SERVICE_ACCOUNT_JSON`.
+  /// Returns the access token as a string.
+  Future<String> _getAccessToken(Session session) async {
     final raw = env['FIREBASE_SERVICE_ACCOUNT_JSON'];
     if (raw == null) {
-      print("FIREBASE_SERVICE_ACCOUNT_JSON not present in env");
+      session.log("ERROR: `FIREBASE_SERVICE_ACCOUNT_JSON` not present in env");
       throw Exception("FIREBASE_SERVICE_ACCOUNT_JSON not present in env");
     }
     final account = json.decode(raw) as Map<String, dynamic>;
@@ -22,7 +28,15 @@ class FcmService {
     return client.credentials.accessToken.data;
   }
 
+  /// Send a notification to a specific device using its FCM token.
+  /// The [session] is implied, but the following variables are required:
+  /// The [token] is the FCM token of the device to send the notification to.
+  /// The [title] is the title of the notification.
+  /// The [body] is the body of the notification.
+  /// The [data] is an optional map of additional data to include in the notification.
+  /// Returns true if the notification was sent successfully, otherwise returns false.
   Future<bool> sendNotification({
+    required Session session,
     required String token,
     required String title,
     required String body,
@@ -30,14 +44,16 @@ class FcmService {
   }) async {
     final raw = env['FIREBASE_SERVICE_ACCOUNT_JSON'];
     if (raw == null) {
-      print("ERROR: `FIREBASE_SERVICE_ACCOUNT_JSON` not found in environment");
+      session.log(
+          "ERROR: `FIREBASE_SERVICE_ACCOUNT_JSON` not found in environment");
       return false;
     }
     final proj = (json.decode(raw) as Map<String, dynamic>)['project_id'];
     final url =
         Uri.parse('https://fcm.googleapis.com/v1/projects/$proj/messages:send');
     final headers = {
-      HttpHeaders.authorizationHeader: 'Bearer ${await _getAccessToken()}',
+      HttpHeaders.authorizationHeader:
+          'Bearer ${await _getAccessToken(session)}',
       HttpHeaders.contentTypeHeader: 'application/json',
     };
     final message = {
